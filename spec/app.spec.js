4b5cfdcca9a2032a9a3b7098abf9725822520582
j-dublon <j.johnson.dublon@gmail.com>
2020-06-10 08:34:06 +0100

    set up initial tests

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -2,3 +2,13 @@ const app = require("../app");
 const request = require("supertest");
 const chai = require("chai");
 const expect = chai.expect;
+
+describe("app", () => {
+  describe("/api/:topic", () => {
+    describe("GET", () => {
+      it("status: 200 returned quotes should have correct keys", () => {
+        return request(app).get("/api/being").expect(200);
+      });
+    });
+  });
+});
