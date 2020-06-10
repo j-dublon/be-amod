8cfb58ab1f4ce8dbef7a23bd22cd1201b325e5d2
j-dublon <j.johnson.dublon@gmail.com>
2020-06-10 10:36:06 +0100

    add migrations folder to db folder

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,9 +1,14 @@
 const { quoteData } = require("../data/index");
 
 exports.seed = function (knex) {
-  return knex
-    .insert(quoteData)
-    .into("quotes")
-    .returning("*")
-    .then((insertedQuotes) => console.log(insertedQuotes));
+  return knex.migrate
+    .rollback()
+    .then(() => knex.migrate.latest())
+    .then(() => {
+      return knex
+        .insert(quoteData)
+        .into("all_quotes")
+        .returning("*")
+        .then((insertedQuotes) => console.log(insertedQuotes));
+    });
 };
