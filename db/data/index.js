f7a3ac3164a53001f392d6f93d4e21ada3f19af4
j-dublon <j.johnson.dublon@gmail.com>
2020-06-26 14:47:55 +0100

    add more quotes to data

--- db/data/index.js
+++ db/data/index.js
@@ -9,6 +9,13 @@ exports.quoteData = [
   {
     topic: "being",
     body:
+      "If we had a keen vision and feeling for all ordinary human life, it would be like hearing the grass grow and the squirrels heart beat, and we should die of that roar which lies on the other side of silence.",
+    author: "George Eliot",
+    work: "Middlemarch",
+  },
+  {
+    topic: "being",
+    body:
       "Now go and make interesting mistakes, make amazing mistakes, make glorious and fantastic mistakes. Break rules. Leave the world more interesting for your being here.",
     author: "Neil Gaiman",
     work: "Make Good Art",
@@ -22,8 +29,28 @@ exports.quoteData = [
   },
   {
     topic: "being",
+    body:
+      "I tell you we are here on earth to fart around and don't let anyone tell you differnet.",
+    author: "Kurt Vonnegut",
+    work: "",
+  },
+  {
+    topic: "being",
     body: "We know the road to freedom has always been stalked by death.",
     author: "Angela Davies",
+    work: "",
+  },
+  {
+    topic: "being",
+    body:
+      "I'd hate to advocate drugs, alcohol or insanity to anyone, but they've always worked for me.",
+    author: "Hunter S. thompson",
+    work: "",
+  },
+  {
+    topic: "being",
+    body: "I am a man and I believe nothing human is foreign to me.",
+    author: "Terence",
     work: "",
   },
   {
@@ -40,10 +67,125 @@ exports.quoteData = [
     work: "The Transformation of Silence into Language & Action",
   },
   {
+    topic: "being",
+    body:
+      "The years like great black oxen tread / And God the herdsman goads them on behind / And I am broken by their passing feet.",
+    author: "W.B.Yeats",
+    work: "The Countess Cathleen",
+  },
+  {
+    topic: "being",
+    body:
+      "It is by universal misunderstanding that all agree. For if, by ill luck, people understood each other, they would never agree.",
+    author: "Baudelaire",
+    work: "",
+  },
+  {
+    topic: "being",
+    body:
+      "I have been here before / But when or how I cannot tell / I know the grass beyond the door / The sweet keen smell / The sighing sound, the lights around the shore.",
+    author: "Dante Gabriel Rossetti",
+    work: "",
+  },
+  {
+    topic: "being",
+    body:
+      "Every act of suffering, no matter how small or agonisingly great, is a test of love in some way.",
+    author: "Gregory David Roberts",
+    work: "",
+  },
+  {
+    topic: "being",
+    body:
+      "At the moment my world caved in on me, when I had nothing left, I discovered I didn't feel depressed, I felt free.",
+    author: "Isabelle Allende",
+    work: "The Infinite Plan",
+  },
+  {
+    topic: "being",
+    body:
+      "I had never been able to make peace between the two halves of myself. All I had managed to do was suppress one half (for a while) at the expense of the other.",
+    author: "Erica Jong",
+    work: "Fear of Flying",
+  },
+  {
+    topic: "being",
+    body:
+      "He'd say I'd crossed the line. Stupid English expressions. What line? Where? Oh yes. The line that you drew between me and everything I want to say and be.",
+    author: "Scarlett Thomas",
+    work: "The End of Mr Y.",
+  },
+  {
+    topic: "being",
+    body:
+      "We are what we pretend to be, so we must be careful about what we pretend to be.",
+    author: "Kurt Vonnegut",
+    work: "Mother Night",
+  },
+  {
+    topic: "being",
+    body:
+      "It is difficult to get a man to understand something when his salary depends on his not understanding it.",
+    author: "Upton Sinclair",
+    work: "I, Candidate for Governor: And how I got licked.",
+  },
+  {
+    topic: "being",
+    body: "Seeing through people is so easy, and it gets you nowhere.",
+    author: "Elias Canetti",
+    work: "",
+  },
+  {
+    topic: "being",
+    body:
+      "Ideals are not measured by whether they conform to reality; reality is judged by whether it lives up to ideals.",
+    author: "Susan Neiman",
+    work: "Moral Clarity,",
+  },
+  {
     topic: "longing",
     body: "Sex is the consolation you have when you can't have love.",
     author: "Gabriel García Márquez",
     work: "One Hundred Years Of Solitude",
+  },
+  {
+    topic: "longing",
+    body:
+      "Some feelings sink so deep within the heart that only loneliness can help you find them again.",
+    author: "Gregory David Roberts",
+    work: "Shantaram,",
+  },
+  {
+    topic: "longing",
+    body: "A heart can be broken, but it keeps on beating, just the same.",
+    author: "Fannie Flagg",
+    work: "Fried Green Tomatoes at the Whistle Stop Cafe",
+  },
+  {
+    topic: "longing",
+    body: "To desire something violently, is to become blind to the rest.",
+    author: "Democritus",
+    work: "",
+  },
+  {
+    topic: "longing",
+    body: "There is such a shelter in each other.",
+    author: "Zadie Smith",
+    work: "On Beauty",
+  },
+  {
+    topic: "longing",
+    body:
+      "You must love as if forever, while knowing that forever would dim the love itself",
+    author: "Susan Neiman",
+    work: "Moral Clarity",
+  },
+  {
+    topic: "longing",
+    body:
+      "You are not what I want, never were, never will be. But I'm a hungry woman. I want more than I want.",
+    author: "Salman Rushdie",
+    work: "The Ground Beneath Her Feet",
   },
   {
     topic: "longing",
@@ -55,9 +197,29 @@ exports.quoteData = [
   {
     topic: "longing",
     body:
+      "But I still wanted to believe what lovers believe; that the thing itself is better than any alternative, be it unrequited, or defeated, or insane.",
+    author: "Salman Rushdie",
+    work: "The Moor's Last Sigh",
+  },
+  {
+    topic: "longing",
+    body:
       "Desire makes everything blossom; possession makes everything wither and fade.",
     author: "Marcel Proust",
     work: "Les Plaisirs et les Jours",
+  },
+  {
+    topic: "longing",
+    body:
+      "Defeated love is still a treasure, and those who choose lovelessness have won no victory at all.",
+    author: "Salman Rushdie",
+    work: "The Moor's Last Sigh",
+  },
+  {
+    topic: "longing",
+    body: "Foolish heart, what do you fear?",
+    author: "Umberto Eco",
+    work: "The Name of the Rose",
   },
   {
     topic: "longing",
